2d1d160415fb4accb7b5498e5c39e174de3cdfb1
Benjamin Tan <165104290+benjamin209086@users.noreply.github.com>
2024-03-31 23:28:15 +0800

    Update carousel.tsx

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getCollectionProducts } from 'lib/shopify';
 import Link from 'next/link';
 import { GridTileImage } from './grid/tile';
